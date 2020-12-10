5499d0a49f583f66464b830d63b194c97a2113e3
AngryPoatt0 <211957142@qq.com>
2020-12-10 19:40:41 +0800

    Memory and Interpreter

--- Interpreter.py
+++ Interpreter.py
@@ -1,9 +1,11 @@
 from Token import*
 from AST import*
+from Memory import*
 
 class Interpreter():
     def __init__(self,ast):
         self.ast = ast
+        self.memory = CallStack()
         self.calcu = {
             '+':    lambda a,b: a + b,
             '-':    lambda a,b: a - b,
@@ -24,6 +26,10 @@ class Interpreter():
             'BinOp':        self.visit_BinOp,
             'UnaryOp':      self.visit_UnaryOp,
             'Num':          self.visit_Num,
+            'Return':       self.visit_Return,
+            'IF':           self.visit_IF,
+            'WHILE':        self.visit_WHILE,
+            'FOR':          self.visit_FOR,
         }
     
     def calculate(self,a,b,op):
@@ -32,32 +38,51 @@ class Interpreter():
     def visit(self,node):
         return self.visiter[type(node).__name__](node)
 
-    def visit_Program(self):
+    def visit_Program(self, node):
+        main_function = [function for function in node.function_list if function.name == 'main']
+        if(main_function != []):
+            main_function = main_function[0]
+        else:
+            raise Exception('Main function cannot found')
+        self.visit(main_function,[])
+
+    def visit_Function(self, node, actual_params):
+        self.memory.push(Frame(node.name,2))
+        current_frame = self.memory.peek()
+        for name, value in zip(node.params, actual_params):
+            current_frame.set_value(name.var_node.value, self.visit(value))
+        return_value = self.visit(node.body)#FIXME:
+        self.pop()
+        return return_value
+
+    def visit_Param(self, node):
         pass
 
-    def visit_Function(self):
+    def visit_Code_Block(self, node):
+        current_frame = self.memory.peek()
+        for decl in node.declarations:
+            current_frame.set_value(decl.var_node.value,0)
+        for statement in node.compound_statement:
+            self.visit(statement)
+            
+
+    def visit_Type(self, node):
         pass
 
-    def visit_Param(self):
+    def visit_Var(self, node):
+        return self.memory.peek().get_value(node.var_node.name)
+
+    def visit_Var_decl(self, node):
         pass
 
-    def visit_Code_Block(self):
-        pass
+    def Assign(self, node):
+        left = node.left.var_node.value
+        right = self.visit(node.right)
+        self.memory.peek().set_value(left,right)
 
-    def visit_Type(self):
-        pass
-
-    def visit_Var(self):
-        pass
-
-    def visit_Var_decl(self):
-        pass
-
-    def Assign(self):
-        pass
-
-    def visit_Function_call(self):
-        pass
+    def visit_Function_call(self, node):
+        actual_params = [param for param in node.real_params]
+        return self.visit_function(func, actual_params)#FIXME:
 
     def visit_BinOp(self, node):
         return self.calcu[node.op.value](self.visit(node.left),self.visit(node.right))
---
+++ Memory.py
@@ -0,0 +1,25 @@
+class Frame():
+    def __init__(self,name,level,type = 'Function'):
+        self.name = name
+        self.level = level
+        self.type = type
+        self.memory = dict()
+
+    def set_value(self,name,value):
+        self.memory[name] = value
+
+    def get_value(self,name):
+        return self.memory[name] if name in self.memory else None
+
+class CallStack():
+    def __init__(self):
+        self.stack = []
+
+    def pop(self):
+        self.stack.pop()
+
+    def push(self,frame):
+        self.stack.append(frame)
+
+    def peek(self):
+        return self.stack[-1]
